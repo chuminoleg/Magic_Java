2d33f4ef6a768feab03f74ff10a83f857dfed0ec
ChuminOleg <oleg.chumin@ah.nl>
2022-06-02 09:47:16 +0300

    Add changes in student's files

--- src/main/java/Students/IliaPlih/Homework2.java
+++ src/main/java/Students/IliaPlih/Homework2.java
@@ -1,27 +1,22 @@
+package Students.IliaPlih;
+
 public class Homework2 {
     public static void main(String[] args) {
-
-
-
-    byte a = 4;
-    short b = 34;
-    int c = 700_000_000;
-    long d = -65466;
-    float e = 8.477f;
-    double y = 67.555;
-    char h = '@';
-    boolean l = true;
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
-    System.out.println(d);
-    System.out.println(e);
-    System.out.println(y);
-    System.out.println(h);
-    System.out.println(l);
-
-
-
-}
-
+        byte a = 4;
+        short b = 34;
+        int c = 700_000_000;
+        long d = -65466;
+        float e = 8.477f;
+        double y = 67.555;
+        char h = '@';
+        boolean l = true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(y);
+        System.out.println(h);
+        System.out.println(l);
+    }
 }
