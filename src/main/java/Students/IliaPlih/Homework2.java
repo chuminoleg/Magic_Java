b2b07c0b857edea21c9a611ac226f041166c4f3e
ChuminOleg <oleg.chumin@ah.nl>
2022-06-02 09:31:58 +0300

    Add second lesson program

---
+++ src/main/java/Students/IliaPlih/Homework2.java
@@ -0,0 +1,27 @@
+public class Homework2 {
+    public static void main(String[] args) {
+
+
+
+    byte a = 4;
+    short b = 34;
+    int c = 700_000_000;
+    long d = -65466;
+    float e = 8.477f;
+    double y = 67.555;
+    char h = '@';
+    boolean l = true;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(y);
+    System.out.println(h);
+    System.out.println(l);
+
+
+
+}
+
+}
